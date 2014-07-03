f362662746aba83e8d1fff25b28292de12f70948
changecase <jeff.ja@gmail.com>
2014-07-03 11:40:42 -0700

    comment out form_validation.js\nI don't think it is even being used

--- source/javascripts/form_validation.js
+++ source/javascripts/form_validation.js
@@ -1,7 +1,7 @@
-var f = document.forms[0],
-    p = document.getElementById("processing");
-
-f.addEventListener("submit", function(e){
-  e.preventDefault();
-  p.style.display = "block";
-});
+// var f = document.forms[0],
+//     p = document.getElementById("processing");
+// 
+// f.addEventListener("submit", function(e){
+//   e.preventDefault();
+//   p.style.display = "block";
+// });
