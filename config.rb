60070466c36eebd9d52bc24cd4aa03814b83084c
changecase <jeff.ja@gmail.com>
2014-08-18 10:40:31 -0700

    fix links, add adv logo

--- config.rb
+++ config.rb
@@ -57,19 +57,23 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-# Moke Bower components importable
+# Make Bower components importable
 sprockets.append_path File.join root, 'bower_components'
+
+# Use directory index files to hide the file extension of pages
+
+activate :directory_indexes
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
