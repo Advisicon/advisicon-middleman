faef8d0a2f761679e1030fbd19ff352579abe6a3
changecase <jeff.ja@gmail.com>
2014-10-01 14:39:32 -0700

    fix impact featurettes, add video

--- source/javascripts/collapse_order_form.js
+++ source/javascripts/collapse_order_form.js
@@ -3,8 +3,12 @@ $(function() {                                                          // load 
   // they have jquery loaded, hide the order form
   $('#order').addClass('collapse');
 
-  $('#expand-form').click(function(){
+  $('.expand-form').click(function(e){
+      e.preventDefault();
       $('#order').collapse('show');                                      // if they want to ship to a shipping destination, show shipping destination form
+      $('html, body').animate({
+        scrollTop: $("#order").offset().top
+      }, 1000);
   });
 
 });
